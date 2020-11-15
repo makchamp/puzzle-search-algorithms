4dfc2dd7d3f6da36499e130822a94114eacdeff7
Khaled Badran <badrankhaled97@gmail.com>
2020-11-15 00:30:28 -0500

    Support dynamically sized puzzles + add tests for moves
    - I changed the representation of the puzzle to a single-dimentional array.
    - I added support for dynamically sized puzzles (any puzzle that is of size 2x2 or bigger) 
    - I added tests for all the moves

--- main.py
+++ main.py
@@ -1,34 +1,81 @@
 from puzzle import Puzzle
 from pprint import pprint
 
-setup_1 = [[0,2,3,1],[4,6,2,5]]
-setup_2 = [[1,2,3,0],[4,6,2,5]]
-setup_3 = [[1,2,3,4],[0,6,2,5]]
-setup_4 = [[1,2,3,4],[5,6,2,0]]
+setup_1 = [0,2,3,1,4,6,2,5]
+setup_2 = [1,2,3,0,4,6,2,5]
+setup_3 = [1,2,3,4,0,6,2,5]
+setup_4 = [1,2,3,4,5,6,2,0]
+setup_5 = [1,2,3,4,5,6,7,0,9,10,11,8]
 
-puzzle = Puzzle(setup_1)
-print(puzzle)
 
-puzzle.move_diagonal_long()
+puzzle = Puzzle(2, setup_1)
+print('\n')
 print(puzzle)
+print(puzzle.possible_moves())
 
-puzzle = Puzzle(setup_2)
-print(puzzle)
 
-puzzle.move_diagonal_long()
+puzzle = Puzzle(2,setup_2)
+print('\n')
 print(puzzle)
+print(puzzle.possible_moves())
 
-puzzle = Puzzle(setup_3)
-print(puzzle)
 
-puzzle.move_diagonal_long()
+puzzle = Puzzle(2,setup_3)
+print('\n')
 print(puzzle)
+print(puzzle.possible_moves())
 
-puzzle = Puzzle(setup_4)
-print(puzzle)
 
-puzzle.move_diagonal_long()
+
+puzzle = Puzzle(2,setup_4)
+print('\n')
 print(puzzle)
+print(puzzle.possible_moves())
+
+
+
+puzzle = Puzzle(2,setup_5)
+print('\n')
+print(puzzle)
+print(puzzle.possible_moves())
+
+
+puzzle = Puzzle(3,setup_5)
+print('\n')
+print(puzzle)
+print(puzzle.possible_moves())
+
+
+
+puzzle = Puzzle(4,setup_5)
+print('\n')
+print(puzzle)
+print(puzzle.possible_moves())
+
+setup_6 = [1, 2, 3, 4, 5, 6, 7, 0, 9, 10, 11, 8]
+puzzle = Puzzle(3,setup_6)
+print('\n')
+print(puzzle)
+print(puzzle.possible_moves())
+
+puzzle = Puzzle(4, [1, 2, 7, 4, 6, 3, 11, 5, 0, 8, 10, 9])
+print(puzzle)
+print(puzzle.possible_moves())
+print(puzzle.is_empty_tile_corner())
+print(puzzle.move_diagonal_short())
+
+# puzzle = Puzzle(6,setup_5)
+# print('\n')
+# print(puzzle)
+#
+# puzzle = Puzzle(1,setup_5)
+# print('\n')
+# print(puzzle)
+#
+# puzzle = Puzzle(12,setup_5)
+# print('\n')
+# print(puzzle)
+
 
 # puzzle.move_right()
 # print(puzzle)
--- puzzle.py
+++ puzzle.py
@@ -1,171 +1,252 @@
 class Puzzle:
 
-    def __init__(self, setup):
+    def __init__(self, rows, setup):
         self.search_path = []
         self.solution_path = []
         self.current_setup = setup
+        if rows < 2:
+            raise Exception('there has to be at least two rows')
+        self.rows: int = rows
+        self.columns: int = int(len(self.current_setup) / self.rows)
+        if self.columns < 2:
+            raise Exception('there has to be at least two columns')
+
         self.total_cost = 0
+        self.goal_setups = [[1, 2, 3, 4, 5, 6, 7, 0], [1, 3, 5, 7, 2, 4, 6, 0]]
 
     def __str__(self):
 
-        tiles = '\n'.join(' '.join(map(str, row)) for row in self.current_setup)
-        # for tile in self.current_setup:
-        #     print(*tile)
+        # divide the long list into rows and columns depending on the puzzle size
+        sublists = []
+        for row in range(0, len(self.current_setup), self.columns):
+            sublists.append(self.current_setup[row:row + self.columns])
+
+        tiles = '\n'.join(' '.join(map(str, row)) for row in sublists)
 
         return tiles
 
+    def possible_moves(self):
+        possible_moves = []
+
+        if self.is_empty_tile_corner():
+            possible_moves.append('move_wrapping')
+            possible_moves.append('move_diagonal_short')
+            possible_moves.append('move_diagonal_long')
+
+        if not self.is_empty_tile_on_right():
+            possible_moves.append('move_right')
+
+        if not self.is_empty_tile_on_left():
+            possible_moves.append('move_left')
+
+        if not self.is_empty_tile_on_bottom():
+            possible_moves.append('move_down')
+
+        if not self.is_empty_tile_on_top():
+            possible_moves.append('move_up')
+
+        return possible_moves
+
+    # Check if the current state is the goal state
+    def is_goal(self):
+
+        return True if self.current_setup in self.goal_setups else False
+
     def is_empty_tile_corner(self):
 
-        if self.current_setup[0][0] == 0 or self.current_setup[1][0] == 0 \
-                or self.current_setup[0][3] == 0 or self.current_setup[1][3] == 0:
-            return True
+        empty_tile_location = self.current_setup.index(0)
+        corners = []
 
-        return False
-
-    def move_vertically(self):
-
-        if 0 in self.current_setup[0]:
-            # find the index of the empty tile and the value of the replacement tile
-            empty_tile_position = self.current_setup[0].index(0)
-            replacement_tile = self.current_setup[1][empty_tile_position]
-
-            # replace the two tiles
-            self.current_setup[0][empty_tile_position] = replacement_tile
-            self.current_setup[1][empty_tile_position] = 0
-
+        if self.rows < 2:
+            corners = [0, len(self.current_setup) - 1]
         else:
+            corners = [0, self.columns - 1, len(self.current_setup) - self.columns, len(self.current_setup) - 1]
 
-            # find the index of the empty tile and the value of the replacement tile
-            empty_tile_position = self.current_setup[1].index(0)
-            replacement_tile = self.current_setup[0][empty_tile_position]
+        # corners = [index for index in range(0, len(self.current_setup))
+        #            if index % self.columns == 0 or index % self.columns == self.columns - 1]
 
-            # replace the two tiles
-            self.current_setup[1][empty_tile_position] = replacement_tile
-            self.current_setup[0][empty_tile_position] = 0
+        return True if empty_tile_location in corners else False
 
-        self.total_cost += 1
-        print("move_vertically applied")
-        return
+    def is_empty_tile_on_right(self):
+
+        empty_tile_location = self.current_setup.index(0)
+
+        right_corners = [index for index in range(0, len(self.current_setup)) if
+                         index % self.columns == self.columns - 1]
+
+        return True if empty_tile_location in right_corners else False
+
+    def is_empty_tile_on_left(self):
+
+        empty_tile_location = self.current_setup.index(0)
+
+        left_corners = [index for index in range(0, len(self.current_setup)) if index % self.columns == 0]
+
+        return True if empty_tile_location in left_corners else False
+
+    def is_empty_tile_on_bottom(self):
+
+        empty_tile_location = self.current_setup.index(0)
+
+        # find the indices of the last row and check if the empty tile is between these indices
+        bottom_row_start_index = len(self.current_setup) - self.columns
+        bottom_row_end_index = len(self.current_setup) - 1
+
+        return True if bottom_row_start_index <= empty_tile_location <= bottom_row_end_index else False
+
+    def is_empty_tile_on_top(self):
+
+        empty_tile_location = self.current_setup.index(0)
+
+        # find the indices of the last row and check if the empty tile is between these indices
+        top_row_start_index = 0
+        top_row_end_index = self.columns - 1
+
+        return True if top_row_start_index <= empty_tile_location <= top_row_end_index else False
+
+    def move_up(self):
+        # this method will create a new state when the empty tile is moved up
+
+        if self.is_empty_tile_on_top():
+            raise Exception('tried to move up when empty tile is already on top row')
+
+        empty_tile_location = self.current_setup.index(0)
+
+        replacement_tile_position = empty_tile_location - self.columns
+
+        # create a new setup and replace the empty tile with the tile above
+        new_setup = self.current_setup
+        new_setup[empty_tile_location] = new_setup[replacement_tile_position]
+        new_setup[replacement_tile_position] = 0
+
+        print("move_up applied")
+        return new_setup
+
+    def move_down(self):
+        # this method will create a new state when the empty tile is moved down
+
+        if self.is_empty_tile_on_bottom():
+            raise Exception('tried to move down when empty tile is already on bottom row')
+
+        empty_tile_location = self.current_setup.index(0)
+        replacement_tile_position = empty_tile_location + self.columns
+
+        # create a new setup and replace the empty tile with the tile above
+        new_setup = self.current_setup
+        new_setup[empty_tile_location] = new_setup[replacement_tile_position]
+        new_setup[replacement_tile_position] = 0
+
+        print("move_down applied")
+        return new_setup
 
     def move_right(self):
+        # this method will create a new state when the empty tile is moved right
 
-        # if 0 is already on the right, then throw an error
-        if self.current_setup[0][3] == 0 or self.current_setup[1][3] == 0:
+        if self.is_empty_tile_on_right():
+            raise Exception('tried to move right when empty tile is already on right corner')
 
-            raise Exception("Tried move_right but the empty tile is already on the right")
+        empty_tile_location = self.current_setup.index(0)
+        replacement_tile_position = empty_tile_location + 1
 
-        else:
+        # create a new setup and replace the empty tile with the tile above
+        new_setup = self.current_setup
+        new_setup[empty_tile_location] = new_setup[replacement_tile_position]
+        new_setup[replacement_tile_position] = 0
 
-            # first find the if the empty tile is on the top or bottom row
-            row = 0 if 0 in self.current_setup[0] else 1
-
-            # find the index of the empty tile and the value of the replacement tile
-            empty_tile_position = self.current_setup[row].index(0)
-            replacement_tile = self.current_setup[row][empty_tile_position + 1]
-
-            # replace the two tiles
-            self.current_setup[row][empty_tile_position] = replacement_tile
-            self.current_setup[row][empty_tile_position + 1] = 0
-
-        self.total_cost += 1
         print("move_right applied")
-        return
+        return new_setup
 
     def move_left(self):
+        # this method will create a new state when the empty tile is moved left
 
-        # if 0 is already on the left, then throw an error
-        if self.current_setup[0][0] == 0 or self.current_setup[1][0] == 0:
+        if self.is_empty_tile_on_left():
+            raise Exception('tried to move left when empty tile is already on left corner')
 
-            raise Exception("Tried move_left but the empty tile is already on the left")
+        empty_tile_location = self.current_setup.index(0)
+        replacement_tile_position = empty_tile_location - 1
 
-        else:
+        # create a new setup and replace the empty tile with the tile above
+        new_setup = self.current_setup
+        new_setup[empty_tile_location] = new_setup[replacement_tile_position]
+        new_setup[replacement_tile_position] = 0
 
-            # first find the if the empty tile is on the top or bottom row
-            row = 0 if 0 in self.current_setup[0] else 1
-
-            # find the index of the empty tile and the value of the replacement tile
-            empty_tile_position = self.current_setup[row].index(0)
-            replacement_tile = self.current_setup[row][empty_tile_position - 1]
-
-            # replace the two tiles
-            self.current_setup[row][empty_tile_position] = replacement_tile
-            self.current_setup[row][empty_tile_position - 1] = 0
-
-        self.total_cost += 1
         print("move_left applied")
-        return
+        return new_setup
 
     def move_wrapping(self):
+        # this method will create a new state when the empty tile is moved to the other corner in the row
 
-        # if 0 is already not on a corner, then throw an error
         if not self.is_empty_tile_corner():
+            raise Exception('tried to move wrapping when empty tile is not on a corner')
 
-            raise Exception("Tried move_wrapper but the empty tile is not on the corner")
+        empty_tile_location = self.current_setup.index(0)
+        replacement_tile_position: int = 0
 
+        if empty_tile_location == 0:
+            replacement_tile_position = self.columns - 1
+        elif empty_tile_location == self.columns - 1:
+            replacement_tile_position = 0
+        elif empty_tile_location == len(self.current_setup) - self.columns:
+            replacement_tile_position = len(self.current_setup) - 1
         else:
+            replacement_tile_position = len(self.current_setup) - self.columns
 
-            # first find the if the empty tile is on the top or bottom row
-            row = 0 if 0 in self.current_setup[0] else 1
+        # create a new setup and replace the empty tile with the replacement tile
+        new_setup = self.current_setup
+        new_setup[empty_tile_location] = new_setup[replacement_tile_position]
+        new_setup[replacement_tile_position] = 0
 
-            # find the indices of the empty and replacement tiles
-            empty_tile_position = self.current_setup[row].index(0)
-            replacement_tile_position = 3 if empty_tile_position == 0 else 0
-            replacement_tile = self.current_setup[row][replacement_tile_position]
-
-            # replace the two tiles
-            self.current_setup[row][empty_tile_position] = replacement_tile
-            self.current_setup[row][replacement_tile_position] = 0
-
-        self.total_cost += 2
         print("move_wrapping applied")
-        return
+        return new_setup
 
     def move_diagonal_short(self):
+        # this method will create a new state when the empty tile is moved diagonally
 
-        # if 0 is already not on a corner, then throw an error
         if not self.is_empty_tile_corner():
+            raise Exception('tried to move_diagonal_short when empty tile is not on a corner')
 
-            raise Exception("Tried move_diagonal_short but the empty tile is not on the corner")
+        empty_tile_location = self.current_setup.index(0)
+        replacement_tile_position = 0
 
-        else:
+        if empty_tile_location == 0:  # top left corner
+            replacement_tile_position = self.columns + 1
+        elif empty_tile_location == self.columns - 1:  # top right corner
+            replacement_tile_position = 2 * self.columns - 2
+        elif empty_tile_location == len(self.current_setup) - self.columns: # bottom left corner
+            replacement_tile_position = len(self.current_setup) - 2*self.columns + 1
+        else:  # bottom right corner
+            replacement_tile_position = len(self.current_setup) - self.columns - 2
 
-            # first find the if the empty tile is on the top or bottom row
-            row = 0 if 0 in self.current_setup[0] else 1
+        # create a new setup and replace the empty tile with the replacement tile
+        new_setup = self.current_setup
+        new_setup[empty_tile_location] = new_setup[replacement_tile_position]
+        new_setup[replacement_tile_position] = 0
 
-            # find the indices of the empty and replacement tiles
-            empty_tile_position = self.current_setup[row].index(0)
-            replacement_tile_position = 1 if empty_tile_position == 0 else 2
-            replacement_tile = self.current_setup[row - 1][replacement_tile_position]
-
-            # replace the two tiles
-            self.current_setup[row][empty_tile_position] = replacement_tile
-            self.current_setup[row - 1][replacement_tile_position] = 0
-
-        self.total_cost += 3
         print("move_diagonal_short applied")
-        return
+        return new_setup
 
     def move_diagonal_long(self):
+        # this method will create a new state when the empty tile is moved to the oppisite corner
 
-        # if 0 is already not on a corner, then throw an error
         if not self.is_empty_tile_corner():
+            raise Exception('tried to move_diagonal_long when empty tile is not on a corner')
 
-            raise Exception("Tried move_diagonal_long but the empty tile is not on the corner")
+        empty_tile_location = self.current_setup.index(0)
+        replacement_tile_position = 0
 
-        else:
+        if empty_tile_location == 0:  # top left corner
+            replacement_tile_position = len(self.current_setup) - 1
+        elif empty_tile_location == self.columns - 1:  # top right corner
+            replacement_tile_position = len(self.current_setup) - self.columns
+        elif empty_tile_location == len(self.current_setup) - self.columns: # bottom left corner
+            replacement_tile_position = self.columns - 1
+        else:  # bottom right corner
+            replacement_tile_position = 0
 
-            # first find the if the empty tile is on the top or bottom row
-            row = 0 if 0 in self.current_setup[0] else 1
+        # create a new setup and replace the empty tile with the replacement tile
+        new_setup = self.current_setup
+        new_setup[empty_tile_location] = new_setup[replacement_tile_position]
+        new_setup[replacement_tile_position] = 0
 
-            # find the indices of the empty and replacement tiles
-            empty_tile_position = self.current_setup[row].index(0)
-            replacement_tile_position = 3 if empty_tile_position == 0 else 0
-            replacement_tile = self.current_setup[row - 1][replacement_tile_position]
-
-            # replace the two tiles
-            self.current_setup[row][empty_tile_position] = replacement_tile
-            self.current_setup[row - 1][replacement_tile_position] = 0
-
-        self.total_cost += 3
         print("move_diagonal_long applied")
-        return
+        return new_setup
